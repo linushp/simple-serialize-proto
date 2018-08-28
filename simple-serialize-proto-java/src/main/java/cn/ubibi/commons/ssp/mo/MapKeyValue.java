52f0fac36b786cc8b4eec6f25ee4b18fc85d3a41
luanhaipeng <luanhaipeng@staff.beechat.io>
2018-08-28 15:28:01 +0800

    java对象序列化

--- simple-serialize-proto-java/src/main/java/cn/ubibi/commons/ssp/mo/MapKeyValue.java
+++ simple-serialize-proto-java/src/main/java/cn/ubibi/commons/ssp/mo/MapKeyValue.java
@@ -5,15 +5,11 @@ import cn.ubibi.commons.ssp.annotation.SimpleSerializeField;
 
 public class MapKeyValue {
 
-//    @SimpleSerializeField(value = 1)
-//    private int valueClassId = 0;
-
     @SimpleSerializeField(value = 2)
     private String key;
 
     @SimpleSerializeField(value = 3)
     private SimpleSerializable value;
-
 
 
     public MapKeyValue(String key, SimpleSerializable value) {
